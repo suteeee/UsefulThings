61f1085b4b44a6fc2fb8805665531618ad7b1e46
sute <72928447+suteeee@users.noreply.github.com>
2023-01-18 14:51:41 +0900

    recycler view swipe menu 관련

---
+++ 안드로이드 RecyclerView Swipe menu 관련/SwipeHelperCallback.kt
@@ -0,0 +1,146 @@
+package com.bigtech.basum.ui.custom
+
+import android.content.ClipData
+import android.graphics.Canvas
+import android.util.Log
+import android.view.View
+import android.view.ViewManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.ACTION_STATE_SWIPE
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.max
+import kotlin.math.min
+
+abstract class SwipeHelperCallback: ItemTouchHelper.Callback() {
+    private var currentPosition: Int? = null
+    private var previousPosition: Int? = null
+    private var currentDx = 0f
+    private var clamp = 0f
+
+    override fun getMovementFlags(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder
+    ): Int {
+        return makeMovementFlags(0, ItemTouchHelper.LEFT)
+    }
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+    }
+
+    override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+        currentDx = 0f
+        previousPosition = viewHolder.adapterPosition
+        getDefaultUIUtil().clearView(getView(viewHolder))
+    }
+
+    override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+        viewHolder?.let {
+            currentPosition = viewHolder.adapterPosition
+            getDefaultUIUtil().onSelected(getView(it))
+        }
+    }
+
+    override fun getSwipeEscapeVelocity(defaultValue: Float): Float {
+        return defaultValue * 10
+    }
+
+    override fun getSwipeThreshold(viewHolder: RecyclerView.ViewHolder): Float {
+        val isClamped = getTag(viewHolder)
+        setTag(viewHolder, !isClamped && currentDx <= -clamp)
+        return 2f
+    }
+
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+
+        if (actionState == ACTION_STATE_SWIPE) {
+            val view = getView(viewHolder)
+            val isClamped = getTag(viewHolder)
+            val x = clampViewPositionHorizontal(view, dX, isClamped, isCurrentlyActive)
+
+            currentDx = x
+            getDefaultUIUtil().onDraw(
+                c,
+                recyclerView,
+                view,
+                x,
+                dY,
+                actionState,
+                isCurrentlyActive
+            )
+        }
+    }
+
+    abstract fun getView(viewHolder: RecyclerView.ViewHolder): View
+
+    private fun setTag(viewHolder: RecyclerView.ViewHolder, isClamped: Boolean) {
+        viewHolder.itemView.tag = isClamped
+    }
+
+    private fun getTag(viewHolder: RecyclerView.ViewHolder): Boolean {
+        return viewHolder.itemView.tag as? Boolean ?: false
+    }
+
+    private fun clampViewPositionHorizontal(
+        view: View,
+        dX: Float,
+        isClamped: Boolean,
+        isCurrentlyActive: Boolean
+    ): Float {
+        val min: Float = -view.width.toFloat()/2
+        val max: Float = 0f
+
+        val x = if (isClamped) {
+            if (isCurrentlyActive) dX - clamp else -clamp
+        } else {
+            dX
+        }
+
+        return min(max(min, x), max)
+    }
+
+    fun setClamp(clamp: Float) {
+        this.clamp = clamp
+    }
+
+    fun removePreviousClamp(recyclerView: RecyclerView){
+        //if (currentPosition == previousPosition) return
+
+        previousPosition?.let {
+            val viewHolder = recyclerView.findViewHolderForAdapterPosition(it) ?: return
+            setTag(viewHolder, false)
+            previousPosition = null
+
+            val x = getView(viewHolder).translationX
+            Log.d("list", "translationX : $x")
+
+            CoroutineScope(Dispatchers.Main).launch {
+                for (i in getView(viewHolder).translationX.toInt() .. 0 step 4) {
+                    delay(1)
+                    getView(viewHolder).translationX = i.toFloat()
+                    Log.d("list", "translationX : ${ getView(viewHolder).translationX}")
+                }
+            }
+        }
+    }
+}
